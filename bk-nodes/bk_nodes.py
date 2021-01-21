271ff08925e38b4453278411d3c231b3f66a44c2
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2021-01-21 17:01:43 -0600

    convert timestamps

--- bk-nodes/bk_nodes.py
+++ bk-nodes/bk_nodes.py
@@ -129,8 +129,11 @@ class State(MethodView):
         except Exception as e:
             raise ErrorResponse(f"Unexpected error: {e}" , status_code=HTTPStatus.INTERNAL_SERVER_ERROR)  
 
-        node_state["timestamp"] = node_state["timestamp"].isoformat()
+        if "timestamp" in node_state:
+            node_state["timestamp"] = node_state["timestamp"].isoformat()
         
+        if "registration_event" in node_state:
+            node_state["registration_event"] = node_state["registration_event"].isoformat()
 
 
         return { "data" : node_state }
