3f1fef468f8e3f7a64fba0c5d0dd49acbd7c2d7c
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2021-03-09 23:26:35 -0600

    remove tty arg

--- unit-tests.sh
+++ unit-tests.sh
@@ -5,7 +5,7 @@ set -x
 
 ### TEST REGISTRATION
 
-JSON=$(docker exec -ti beekeeper_bk-sshd_1 bash -c 'ssh -o StrictHostKeyChecking=no  sage_registration@bk-sshd -p 22 -i /usr/lib/waggle/registration_keys/registration register 0000000000000001')
+JSON=$(docker exec -i beekeeper_bk-sshd_1 bash -c 'ssh -o StrictHostKeyChecking=no  sage_registration@bk-sshd -p 22 -i /usr/lib/waggle/registration_keys/registration register 0000000000000001')
 echo $JSON
 NEW_ID=$(echo ${JSON} | jq -r -j .id | tail -n 1)
 
@@ -18,7 +18,7 @@ echo "correct node_id was returned in registration process"
 
 # verify node is in database
 
-NEW_ID2=$(docker exec -ti beekeeper_bk-api_1  ash -c "curl localhost:5000/state/0000000000000001" | jq -r -j .data.id)
+NEW_ID2=$(docker exec -i beekeeper_bk-api_1  ash -c "curl localhost:5000/state/0000000000000001" | jq -r -j .data.id)
 if [ "${NEW_ID2}_" != "0000000000000001_" ] ; then
   echo "database test failed, expected \"0000000000000001\", got \"${NEW_ID2}\""
   exit 1
