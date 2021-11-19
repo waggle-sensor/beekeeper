0b4eb4202ae04c51a83976012727e4471e561b2a
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2021-11-19 14:45:29 -0600

    5 minute loop

--- bk-deploy-manager/deploy_manager.py
+++ bk-deploy-manager/deploy_manager.py
@@ -18,10 +18,20 @@ def parseTime(timestamp):
     return dateutil.parser.isoparse(timestamp)
 
 def get_candidates():
-    url =  f"{BEEKEEPER_URL}/state"
 
+    if BEEKEEPER_URL == "":
+        print(f"BEEKEEPER_URL not defined")
+        sys.exit(1)
 
-    resp  = requests.get(url)
+    print(f"BEEKEEPER_URL: {BEEKEEPER_URL}")
+    url = f'{BEEKEEPER_URL}/state'
+    print(f"url: {url}")
+
+    try:
+        resp  = requests.get(url)
+    except Exception as e:
+        raise Exception(f"GET request to {url} failed: {str(e)}")
+
 
     if resp.status_code != 200:
         raise Exception(f"status_code: {resp.status_code} body: {resp.text}")
@@ -38,14 +48,15 @@ def get_candidates():
         node_id  = n["id"]
         #print("id: "+node_id)
         #print("wes_deploy_event: "+n["wes_deploy_event"])
-        if not "wes_deploy_event" in n:
+        if n.get("beehive") in ["", None]:
+            print(f"node {node_id} does not belong to a beehive")
+            continue
+
+        if n.get("wes_deploy_event") in ["", None]:
             print(f"scheduling node {node_id} for wes deployment (reason: no previous deployment)")
             candidates.append(n)
             continue
-        if n["wes_deploy_event"] == None:
-            print(f"scheduling node {node_id} for wes deployment (reason: no previous deployment)")
-            candidates.append(n)
-            continue
+
         ts = parseTime(n["wes_deploy_event"])
 
         ts_diff = datetime.datetime.utcnow() - ts
@@ -59,36 +70,52 @@ def get_candidates():
     return candidates
 
 
-candidates = get_candidates()
+def try_wes_deployment(candidates):
 
-if len(candidates) == 0:
-    print("no candidates for wes deployment found")
+
+    success_count = 0
+
+    for n in candidates:
+        node_id  = n["id"]
+        #curl localhost:5000/node/0000000000000001 -d '{"deploy_wes": true}'
+        d_url = f"{BEEKEEPER_URL}/node/{node_id}"
+        resp = requests.post(d_url, json={"deploy_wes":True})
+        if resp.status_code != 200:
+            print(d_url)
+            print(f"Something went wrong: status_code: {resp.status_code} body: {resp.text}")
+        result = resp.json()
+        if not "success" in result:
+            print(d_url)
+            print(f"Something went wrong: status_code: {resp.status_code} body: {resp.text}")
+        if not result["success"]:
+            print(d_url)
+            print(f"Something went wrong: status_code: {resp.status_code} body: {resp.text}")
+
+        success_count += 1
+        time.sleep(2)
+
+    print(f"{success_count} out of {len(candidates)} successful.")
     print("done")
-    sys.exit(0)
 
-print("candidates:")
-print(candidates)
+    return
 
-success_count = 0
 
-for n in candidates:
-    node_id  = n["id"]
-    #curl localhost:5000/node/0000000000000001 -d '{"deploy_wes": true}'
-    d_url = f"{BEEKEEPER_URL}/node/{node_id}"
-    resp = requests.post(d_url, json={"deploy_wes":True})
-    if resp.status_code != 200:
-        print(d_url)
-        print(f"Something went wrong: status_code: {resp.status_code} body: {resp.text}")
-    result = resp.json()
-    if not "success" in result:
-        print(d_url)
-        print(f"Something went wrong: status_code: {resp.status_code} body: {resp.text}")
-    if not result["success"]:
-        print(d_url)
-        print(f"Something went wrong: status_code: {resp.status_code} body: {resp.text}")
+while True:
 
-    success_count += 1
-    time.sleep(2)
+    candidates = []
+    try:
+        candidates = get_candidates()
+    except Exception as e:
+        print(f"error: get_candidates returned: {str(e)}")
 
-print(f"{success_count} out of {len(candidates)} successful.")
-print("done")
+    if len(candidates) == 0:
+        print("no candidates for wes deployment found")
+    else:
+        print("candidates:")
+        print(candidates)
+        try_wes_deployment(candidates)
+
+    print("waiting 5 minutes...")
+    time.sleep(5*60)
+
+
